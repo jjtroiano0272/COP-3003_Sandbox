071d4ecc5056f8656b085effc3d62102e86d2b78
jjtroiano0272 <jjtroiano0272@eagle.fgcu.edu>
2018-10-10 11:13:50 -0400

    Added GenericStack.

---
+++ src/GenericStack.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+/*
+* Created: 10/10/2018
+* */
+
+public class GenericStack {
+	// Represents the location [subscript] of the top of the stack.
+	private int top;
+	// GENERIC ArrayList
+	private ArrayList<T> items = new ArrayList<T>(); // T must be defined.
+	ArrayList<T> items;
+	
+	public ArrayStack(int maxSize) {
+		items = new arrayList<>(); // The fuck am I doing in this file...
+	}
+	
+	public GenericStack() {}
+	
+	public int pop() {
+	
+	}
+	public int push() P{;}
+}
